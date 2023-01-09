562815f3694855468fbe3516bf8ad95040745658
GContain <ii76030322@gmail.com>
2023-01-09 13:41:31 +0900

    model Mountain

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Mountain(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    address = models.TextField()
+    latitude = models.CharField(max_length=50) # 위도
+    hardness = models.CharField(max_length=50) # 경도
+    info = models.TextField()
+    img_url = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
