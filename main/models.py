37e1546bdc402e5b2b7481e6f3dba62f7c0691e2
GContain <ii76030322@gmail.com>
2023-01-10 11:12:32 +0900

    model c_Mountain_img, m_Mountain

--- main/models.py
+++ main/models.py
@@ -8,9 +8,17 @@ class Mountain(models.Model):
     latitude = models.CharField(max_length=50) # 위도
     longitude = models.CharField(max_length=50) # 경도
     info = models.TextField()
-    img_url = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
+
+class Mountain_img(models.Model):
+    mountain = models.ForeignKey(Mountain, on_delete=models.CASCADE)
+    img_url = models.CharField(max_length=50)
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.mountain
