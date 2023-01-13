973d9ba9a91d05f58789046ea3057703866a39de
GContain <ii76030322@gmail.com>
2023-01-13 13:20:38 +0900

    Add search.html

--- main/models.py
+++ main/models.py
@@ -8,8 +8,6 @@ class Mountain(models.Model):
     latitude = models.CharField(max_length=50) # 위도
     longitude = models.CharField(max_length=50) # 경도
     info = models.TextField()
-    create_date = models.DateTimeField()
-    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
@@ -17,8 +15,6 @@ class Mountain(models.Model):
 class Mountain_img(models.Model):
     mountain = models.ForeignKey(Mountain, on_delete=models.CASCADE)
     img_url = models.CharField(max_length=50)
-    create_date = models.DateTimeField()
-    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.mountain
